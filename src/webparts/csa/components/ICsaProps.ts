a8ce9045484720029c3ed053eb47affed62cd6b8
Srinivas Narula <Srinivas.Narula@mnscorp.net>
2020-05-24 22:43:40 +0100

    Plain vanilla working version

---
+++ src/webparts/csa/components/ICsaProps.ts
@@ -0,0 +1,3 @@
+export interface ICsaProps {
+  description: string;
+}
---
+++ src/webparts/csa/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface ICsaWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'CsaWebPartStrings' {
+  const strings: ICsaWebPartStrings;
+  export = strings;
+}
