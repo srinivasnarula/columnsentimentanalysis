e8296a2e0b54179be299fc9f4094eb6998f8f59f
Srinivas Narula <Srinivas.Narula@mnscorp.net>
2020-05-24 22:37:36 +0100

    file missing

--- lib/webparts/columnSentimentAnalysis/components/ColumnSentimentAnalysis.d.ts
+++
@@ -1,6 +0,0 @@
-import * as React from 'react';
-import { IColumnSentimentAnalysisProps } from './IColumnSentimentAnalysisProps';
-export default class ColumnSentimentAnalysis extends React.Component<IColumnSentimentAnalysisProps, {}> {
-    render(): React.ReactElement<IColumnSentimentAnalysisProps>;
-}
-//# sourceMappingURL=ColumnSentimentAnalysis.d.ts.map
--- lib/webparts/columnSentimentAnalysis/components/IColumnSentimentAnalysisProps.d.ts
+++
@@ -1,4 +0,0 @@
-export interface IColumnSentimentAnalysisProps {
-    description: string;
-}
-//# sourceMappingURL=IColumnSentimentAnalysisProps.d.ts.map
